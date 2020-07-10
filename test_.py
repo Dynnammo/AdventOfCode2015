c1369780885ed24ff8dd65dd5c17f45884987eaa
Dynnammo <33259633+Dynnammo@users.noreply.github.com>
2020-07-10 16:19:11 +0200

    Resolve day 3 (#2)

--- test_.py
+++ test_.py
@@ -1,18 +1,22 @@
 from json import loads
 
-from day1 import (find_floor, find_basement_entry_char)
+from day1 import find_floor, find_basement_entry_char
 from day2 import (
     compute_paper_need,
     total_paper_needed,
     compute_ribbon_need,
     total_ribbon_needed
 )
+from day3 import (
+    number_of_distributed_presents,
+    number_of_distributed_presents_with_robot
+)
 
 answers = loads(open('answers.json').read())
 
 
 def test_day_1():
-    inputDay1 = open('inputs/day1', 'r').read()
+    input_day_1 = open('inputs/day1', 'r').read()
     assert find_floor('(())') == 0
     assert find_floor('()()') == 0
     assert find_floor('(((') == 3
@@ -22,11 +26,11 @@ def test_day_1():
     assert find_floor('))(') == -1
     assert find_floor(')))') == -3
     assert find_floor(')())())') == -3
-    assert find_floor(inputDay1) == answers['day1']['part1']
+    assert find_floor(input_day_1) == answers['day1']['part1']
 
     assert find_basement_entry_char(')') == 1
     assert find_basement_entry_char('()())') == 5
-    assert find_basement_entry_char(inputDay1) == answers['day1']['part2']
+    assert find_basement_entry_char(input_day_1) == answers['day1']['part2']
 
 
 def test_day_2():
@@ -39,3 +43,20 @@ def test_day_2():
     assert compute_ribbon_need(2, 3, 4) == 34
     assert compute_ribbon_need(1, 1, 10) == 14
     assert total_ribbon_needed(input_day_2) == answers['day2']['part2']
+
+
+def test_day_3():
+    input_day_3 = open('inputs/day3', 'r').read()
+    assert number_of_distributed_presents('>') == 2
+    assert number_of_distributed_presents('^>v<') == 4
+    assert number_of_distributed_presents('^v^v^v^v^v') == 2
+
+    assert number_of_distributed_presents(input_day_3) == \
+        answers['day3']['part1']
+
+    assert number_of_distributed_presents_with_robot('^>') == 3
+    assert number_of_distributed_presents_with_robot('^>v<') == 3
+    assert number_of_distributed_presents_with_robot('^v^v^v^v^v') == 11
+
+    assert number_of_distributed_presents_with_robot(input_day_3) == \
+        answers['day3']['part2']
