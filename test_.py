76dbed1e83a139c2b13f6436dd2b654b92251cbe
dynnammo <baptiste.thivend@insa-lyon.fr>
2020-07-10 15:31:22 +0200

    Resolve day2, hide answers in tests in answer.json file

--- test_.py
+++ test_.py
@@ -1,19 +1,41 @@
-from day1 import (findFloor, findBasementEntryChar)
+from json import loads
+
+from day1 import (find_floor, find_basement_entry_char)
+from day2 import (
+    compute_paper_need,
+    total_paper_needed,
+    compute_ribbon_need,
+    total_ribbon_needed
+)
+
+answers = loads(open('answers.json').read())
 
 
 def test_day_1():
     inputDay1 = open('inputs/day1', 'r').read()
-    assert findFloor('(())') == 0
-    assert findFloor('()()') == 0
-    assert findFloor('(((') == 3
-    assert findFloor('(()(()(') == 3
-    assert findFloor('))(((((') == 3
-    assert findFloor('())') == -1
-    assert findFloor('))(') == -1
-    assert findFloor(')))') == -3
-    assert findFloor(')())())') == -3
-    assert findFloor(inputDay1) == 232
+    assert find_floor('(())') == 0
+    assert find_floor('()()') == 0
+    assert find_floor('(((') == 3
+    assert find_floor('(()(()(') == 3
+    assert find_floor('))(((((') == 3
+    assert find_floor('())') == -1
+    assert find_floor('))(') == -1
+    assert find_floor(')))') == -3
+    assert find_floor(')())())') == -3
+    assert find_floor(inputDay1) == answers['day1']['part1']
 
-    assert findBasementEntryChar(')') == 1
-    assert findBasementEntryChar('()())') == 5
-    assert findBasementEntryChar(inputDay1) == 1783
+    assert find_basement_entry_char(')') == 1
+    assert find_basement_entry_char('()())') == 5
+    assert find_basement_entry_char(inputDay1) == answers['day1']['part2']
+
+
+def test_day_2():
+    f = open('./inputs/day2', 'r').read()
+    input_day_2 = f.split('\n')
+    assert compute_paper_need(2, 3, 4) == 58
+    assert compute_paper_need(1, 1, 10) == 43
+    assert total_paper_needed(input_day_2) == answers['day2']['part1']
+
+    assert compute_ribbon_need(2, 3, 4) == 34
+    assert compute_ribbon_need(1, 1, 10) == 14
+    assert total_ribbon_needed(input_day_2) == answers['day2']['part2']
